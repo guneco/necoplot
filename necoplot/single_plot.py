bd8f3bab753919e6d77b69d68dcb9d3416e0c433
guneco <gu3fav@gmail.com>
2022-05-06 08:47:32 +0900

    Allow plot() directry access ax

--- necoplot/single_plot.py
+++ necoplot/single_plot.py
@@ -1,59 +1,32 @@
 from typing import Callable
 
 import matplotlib.pyplot as plt
-
 from matplotlib.axes._axes import Axes
 
 from .core_plot import *
 
-class NecoAxes(Axes):
-    def __init__(self):
-        self.fig = plt.figure(figsize=(5,3))
+def __enter__(self):
+    return(self)
 
-    def __enter__(self):
-        return(self)
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        plt.show()
-    
+def __exit__(self, exc_type, exc_value, exc_traceback):
+    plt.show()
 
-# class SinglePlot():
-#     def __init__(self):
-#         self.fig = plt.figure(figsize=(5,3))
-#         self._ax = None
 
-#     def __enter__(self):
-#         return(self)
+Axes.__enter__ = __enter__
+Axes.__exit__ = __exit__
 
-#     def __exit__(self, exc_type, exc_value, exc_traceback):
-#         plt.show()
-
-#     def plot(self):
-#         self._ax
-        
-#     @property
-#     def ax(self):
-#         return self._ax
-    
-#     @ax.setter
-#     def ax(self, ax):
-#         self._ax = ax
-    
 
 def plot(ax_func: Callable = None,):
-    cp = CorePlot()
-    ax = cp.add_subplot(111)
-    cp.ax = ax
-
+    fig = plt.figure(figsize=(5,3))
+    ax = fig.add_subplot(111)
+    
     if not ax_func:
         ax_func = set_ax()
     
     ax = ax_func(ax)
         
-    cp.ax = ax
-    # return ax
-    return cp
-
+    return ax
 
 
 def set_ax(xlim: list[float] = None):

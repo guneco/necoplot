68f1fc6979115896909bbcfa1f2be85eb9272f96
guneco <gu3fav@gmail.com>
2022-05-07 16:43:12 +0900

    fix apply_user_parameters because dict update did not work

--- necoplot/single_plot.py
+++ necoplot/single_plot.py
@@ -1,11 +1,14 @@
-from typing import Callable, Optional
 import inspect
+import itertools
+from typing import Callable, Optional
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes._axes import Axes
 
 from necoplot.extract_params import extract_params
+import necoplot.config as config
+
 
 def __enter__(self):
     return(self)
@@ -20,6 +23,28 @@ FIGURE_PARAMS = extract_params(Figure)
 AXES_PARAMS = extract_params(Axes)
 
 
+def apply_user_parameters(target_kwargs:list[str]):
+    
+    if isinstance(target_kwargs[0], list):
+        target_kwargs = list(itertools.chain.from_iterable(target_kwargs))
+    
+    def _apply_config(func):
+        
+        def wrapper(*args, **kwargs):
+            
+            parameter = {key:value for key, value in config.user_parameters.items() if key in target_kwargs}
+            kwargs = {**parameter, **kwargs}
+            
+            result = func(*args, **kwargs)
+            
+            return result
+        
+        return wrapper
+    
+    return _apply_config
+
+
+@apply_user_parameters([FIGURE_PARAMS, AXES_PARAMS])
 def plot(
     ax_config: Optional[Callable] = None,
     figsize: tuple[float, float] = (6,4),
@@ -33,7 +58,7 @@ def plot(
     
     for key in kwargs:
         if (key not in FIGURE_PARAMS) and (key not in AXES_PARAMS):
-            print(f'Info: {key} is ignored because it is not found in FIGURE_PARAMS or AXES_PARAMS')
+            print(f'CAUTION: {key} is ignored because it is not found in FIGURE_PARAMS or AXES_PARAMS')
 
     fig = plt.figure(figsize=figsize, dpi=dpi, layout=layout, **figure_params)
     
@@ -43,16 +68,57 @@ def plot(
     return ax
 
 
+# def config_ax(
+#     title: Optional[str] = None,
+#     xlabel: Optional[str] = None,
+#     ylabel: Optional[str] = None,
+#     xlim: Optional[tuple[float, float]] = None,
+#     ylim: Optional[tuple[float, float]] = None,
+#     xticks: Optional[list] = None,
+#     yticks: Optional[list] = None,
+#     xticklabels: Optional[str] = None,
+#     yticklabels: Optional[str] = None,
+#     **kwargs) -> Callable:
+#     """Return a function to config ax with keyword args"""
+    
+#     kwargs_not_packed = _get_kwargs_in_this_function_as_dict()
+#     all_kwargs = {**kwargs_not_packed, **kwargs}
+    
+#     def _config_ax(fig):
+        
+#         ax = fig.add_subplot(111, **all_kwargs)
+        
+#         return ax
+
+#     return _config_ax
+
+
+# def _get_kwargs_in_this_function_as_dict() -> dict:
+#     """Return parent function args and values as dict"""
+#     fname = inspect.currentframe().f_back
+#     args_dict = inspect.getargvalues(fname).locals
+    
+#     if 'kwargs' in args_dict.keys():
+#         del args_dict['kwargs']
+    
+#     args_dict = {k: v for k, v in args_dict.items() if v is not None}
+    
+#     return args_dict
+
+
+def save(fname: str, show=True, **kwargs) -> None:
+    """Save a figure with keyword args"""
+    plt.savefig(fname, **kwargs)
+    plt.close() if not show else None
+    
+
+
+
+@apply_user_parameters(AXES_PARAMS)
 def config_ax(
     title: Optional[str] = None,
     xlabel: Optional[str] = None,
     ylabel: Optional[str] = None,
-    xlim: Optional[tuple[float, float]] = None,
-    ylim: Optional[tuple[float, float]] = None,
-    xticks: Optional[list] = None,
-    yticks: Optional[list] = None,
-    xticklabels: Optional[str] = None,
-    yticklabels: Optional[str] = None,
     **kwargs) -> Callable:
     """Return a function to config ax with keyword args"""
     
@@ -81,30 +147,29 @@ def _get_kwargs_in_this_function_as_dict() -> dict:
     return args_dict
 
 
-def save(fname: str, show=True, **kwargs) -> None:
-    """Save a figure with keyword args"""
-    plt.savefig(fname, **kwargs)
-    plt.close() if not show else None
+def config_user_parameters(**kwargs):
+    config.user_parameters.update(kwargs)
     
 
-default_dict = {}
+def reset():
+    config.user_parameters = {}
 
-def config_default_dict(**kwargs):
-    default_dict.update(kwargs)
+# def config_default_dict(**kwargs):
+#     config.user_settings.update(kwargs)
 
-def apply_config(target_kwargs:list):
-    default_dict = {key: default_dict[key] for key in default_dict.keys() if key in target_kwargs}
+# def apply_config(target_kwargs:list):
+#     default_dict = {key: default_dict[key] for key in default_dict.keys() if key in target_kwargs}
     
     
-    def _apply_config(func):
+#     def _apply_config(func):
         
-        def wrapper(*args, **kwargs):
-            default_dict.update(kwargs)
+#         def wrapper(*args, **kwargs):
+#             default_dict.update(kwargs)
             
-            result = func(*args, **kwargs)
+#             result = func(*args, **kwargs)
             
-            return result
+#             return result
         
-        return wrapper
+#         return wrapper
     
-    return _apply_config
+#     return _apply_config

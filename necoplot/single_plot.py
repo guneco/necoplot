b429d02de00b839fb1fb2e96bc3ce5a0c63cdfd6
guneco <gu3fav@gmail.com>
2022-05-07 09:41:19 +0900

    Add parameters to cinfig_ax

--- necoplot/single_plot.py
+++ necoplot/single_plot.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional
 
 import matplotlib.pyplot as plt
 from matplotlib.axes._axes import Axes
@@ -13,90 +13,64 @@ def __exit__(self, exc_type, exc_value, exc_traceback):
 Axes.__enter__ = __enter__
 Axes.__exit__ = __exit__
 
+FIGURE_ARGS = [arg.replace('set_', '') for arg in dir(plt.figure()) if not arg.startswith('_')]
+AXES_ARGS = [arg.replace('set_', '') for arg in dir(Axes) if not arg.startswith('_')]
+
 
 def plot(
-    ax_config: Callable = None,
-    figsize: tuple[float] = (6,4),
+    ax_config: Optional[Callable] = None,
+    figsize: tuple[float, float] = (6,4),
     dpi: int = 300,
+    layout: str = 'tight',
     **kwargs):
+    """Plot a figure with setting figure args and axes args"""
     
-    ax_config = ax_config if ax_config else config_ax()
+    figure_args = {key: kwargs[key] for key in kwargs.keys() if key in FIGURE_ARGS}
+    axes_args = {key: kwargs[key] for key in kwargs.keys() if key in AXES_ARGS}
+    
+    for key in kwargs:
+        if (key not in FIGURE_ARGS) and (key not in AXES_ARGS):
+            print(f'Info: {key} is ignored because it is not found in FIGURE_ARGS or AXES_ARGS')
 
-    fig = plt.figure(figsize=figsize, dpi=dpi)
-    # ax = fig.add_subplot(111)
-        
-    ax = ax_config(fig)
+    fig = plt.figure(figsize=figsize, dpi=dpi, layout=layout, **figure_args)
     
-    if kwargs:
-        add_config = add_config_ax(**kwargs)
-        ax = add_config(ax)
-        
+    ax_config = ax_config if ax_config else config_ax(**axes_args)
+    ax = ax_config(fig)
+            
     return ax
 
 
 def config_ax(
-    # xlim: tuple[float] = None,
-    # ylim: tuple[float] = None,
-    **kwagrs
-    ):
+    title: str = None,
+    xlabel: str = None,
+    ylabel: str = None,
+    xlim: tuple[float, float] = None,
+    ylim: tuple[float, float] = None,
+    xticks: list = None,
+    yticks: list = None,
+    xticklabels: str = None,
+    yticklabels: str = None,
+    **kwagrs):
+    """Return a function to config ax with keyword args"""
     
     def _config_ax(fig):
-        ax = fig.add_subplot(111, **kwagrs)
-        # ax.set_xlim(xlim) if xlim else None
-        # ax.set_ylim(ylim) if xlim else None
-        return ax
-
-    return _config_ax
-
-def add_config_ax(
-    xlim: tuple[float] = None,
-    ylim: tuple[float] = None,
-    **kwargs
-    ):
-    
-    def _config_ax(ax):
-        ax.set_xlim(xlim) if xlim else None
-        ax.set_ylim(ylim) if ylim else None
+        ax = fig.add_subplot(111, 
+            title,
+            xlabel,
+            ylabel,
+            xlim,
+            ylim,
+            xticks,
+            yticks,
+            xticklabels,
+            yticklabels,
+            **kwagrs)
         return ax
 
     return _config_ax
 
 
-
-# def plot(
-#     ax_config: Callable = None,
-#     figsize: tuple[float] = (6,4),
-#     dpi: int = 300,
-#     save_path:str = None,
-#     **kwargs):
-    
-#     ax_config = ax_config if ax_config else config_ax()
-
-#     fig = plt.figure(figsize=figsize, dpi=dpi)
-#     ax = fig.add_subplot(111, ylim=(0,1))
-        
-#     ax = ax_config(ax)
-    
-#     if kwargs:
-#         add_config = config_ax(**kwargs)
-#         ax = add_config(ax)
-        
-#     return ax
-
-
-# def config_ax(
-#     xlim: tuple[float] = None,
-#     ylim: tuple[float] = None,
-    
-#     ):
-    
-#     def _config_ax(ax):
-#         ax.set_xlim(xlim) if xlim else None
-#         ax.set_ylim(ylim) if xlim else None
-#         return ax
-
-#     return _config_ax
-
-
-
-
+def save(fname: str, show=True, **kwargs):
+    """Save a figure with keyword args"""
+    plt.savefig(fname, **kwargs)
+    plt.close() if not show else None

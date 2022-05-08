a19469fdf38554257b2dcdc5bdacd56eb29ad7d0
guneco <gu3fav@gmail.com>
2022-05-08 10:36:44 +0900

    Update mplot to fit context_ax

--- necoplot/multi_plot.py
+++ necoplot/multi_plot.py
@@ -4,48 +4,49 @@ from typing import Callable
 
 import matplotlib.pyplot as plt
 
-from matplotlib.axes._axes import Axes
+import necoplot.common as common
+from necoplot.extract_params import FIGURE_PARAMS
+from necoplot.common import config_ax, config_user_parameters
 
-import matplotlib.pyplot as plt
 
-
-class PlotBase():
+class PlotBase:
+    """Basement class for multi plot"""
     def __init__(self,
         figsize: tuple[float, float] = (6,4),
         dpi: int = 300,
         layout: str = 'tight',
-        show=True,
+        show: bool =True,
         **kwagrs):
         
-        self.fig = plt.figure(
-            figsize=figsize, dpi=dpi, layout=layout, **kwagrs
-            )
+        self.fig = plt.figure(figsize=figsize, dpi=dpi, layout=layout, **kwagrs)
         self.show = show
+        self.axes = None
 
     def __enter__(self):
         return(self)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         plt.show() if self.show else None
-        
-
-
-def mplot(ax_funcs: list[Callable], indices: int):
-    pb = PlotBase()
-    axes = []
+      
+  
+@common._apply_user_parameters(FIGURE_PARAMS)
+def mplot(
+    ax_funcs: list[Callable],
+    figsize=(6,4),
+    dpi: int = 300,
+    layout: str = 'tight',
+    show: bool =True,
+    **kwargs
+    ):
+    """Context manager for mult plot"""
+    ax_funcs = [ax_funcs] if isinstance(ax_funcs, Callable) else ax_funcs
     
-    for func, index in zip(ax_funcs, indices):
-        ax = func(ax, index_=index)
+    pb = PlotBase(figsize=figsize, dpi=dpi, layout=layout, **kwargs)
+    axes = []
+        
+    for func in ax_funcs:
+        ax = func(pb.fig)
         axes.append(ax)
         
     pb.axes = axes
     return pb
-
-
-def set_ax(xlim: list[float] = None):
-    
-    def _set_ax(ax):
-        ax.set_xlim(*xlim) if xlim else None
-        return ax
-
-    return _set_ax

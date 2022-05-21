82997edf9df78f02b387c5a8f6707728afb3eb3d
guneco <gu3fav@gmail.com>
2022-05-22 06:02:33 +0900

    Move PlotBase to refactor

---
+++ necoplot/plot_base.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+
+class PlotBase:
+    """Basement class for a plot"""
+    def __init__(self,
+        figsize: tuple[float, float] = (6,4),
+        dpi: int = 150,
+        layout: str = 'tight',
+        show: bool =True,
+        **kwagrs):
+        
+        self.fig = plt.figure(figsize=figsize, dpi=dpi, layout=layout, **kwagrs)
+        self.show = show
+        self.axes = None
+
+    def __enter__(self):
+        return(self)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        plt.show() if self.show else None

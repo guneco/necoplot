88f8223d2a4db081979ed3a51f02907dd6a1ac7e
guneco <gu3fav@gmail.com>
2022-05-22 06:07:34 +0900

    replace plotbase

--- necoplot/multi_plot.py
+++ necoplot/multi_plot.py
@@ -5,29 +5,10 @@ from typing import Callable
 import matplotlib.pyplot as plt
 
 import necoplot.common as common
+from necoplot.plot_base import PlotBase
 from necoplot.extract_params import FIGURE_PARAMS
 from necoplot.common import config_ax, config_user_parameters
 
-
-class PlotBase:
-    """Basement class for multi plot"""
-    def __init__(self,
-        figsize: tuple[float, float] = (6,4),
-        dpi: int = 300,
-        layout: str = 'tight',
-        show: bool =True,
-        **kwagrs):
-        
-        self.fig = plt.figure(figsize=figsize, dpi=dpi, layout=layout, **kwagrs)
-        self.show = show
-        self.axes = None
-
-    def __enter__(self):
-        return(self)
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        plt.show() if self.show else None
-      
   
 @common._apply_user_parameters(FIGURE_PARAMS)
 def mplot(

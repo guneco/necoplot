bd5eb6d6b5509de5d9a78dabd147e7e9cb3234f8
guneco <gu3fav@gmail.com>
2022-05-22 06:56:03 +0900

    Make a minimal plot function

--- necoplot/slope_plot.py
+++ necoplot/slope_plot.py
@@ -13,22 +13,39 @@ from necoplot.common import config_ax, config_user_parameters
 class Slope(PlotBase):
     """Class for a slope chart"""
     def __init__(self,
+        time0: list[float],
+        time1: list[float],
+        names: list[str],
         figsize: tuple[float, float] = (6,4),
         dpi: int = 150,
         layout: str = 'tight',
         show: bool =True,
         **kwagrs):
-        super().__init__(figsize=figsize, dpi=dpi, layout=layout, show=show, **kwagrs)
-
+        super().__init__(
+            figsize=figsize, dpi=dpi, layout=layout, 
+            show=show, **kwagrs)
+        self.time0 = time0
+        self.time1 = time1
+        self.names = names
+        
     def __enter__(self):
         return(self)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        
+        for t0, t1, name in zip(self.time0, self.time1, self.names):
+            plt.plot([0,1],[t0, t1])
+        
         plt.show() if self.show else None
-      
-  
-@common._apply_user_parameters(FIGURE_PARAMS)
+        
+    def highlight(self, target_dict: dict):
+        pass
+        
+# @common._apply_user_parameters(FIGURE_PARAMS)
 def slope(
+    time0: list[float],
+    time1: list[float],
+    names: list[str],
     figsize=(6,4),
     dpi: int = 150,
     layout: str = 'tight',
@@ -37,7 +54,7 @@ def slope(
     ):
     """Context manager for a slope chart"""
     
-    slp = Slope(figsize=figsize, dpi=dpi, layout=layout, show=show **kwargs)
-    
+    slp = Slope(time0, time1, names, figsize=figsize, dpi=dpi, layout=layout, show=show, **kwargs)
 
     return slp
+

288660f6794721ad8fc98d68262b4d0e91430482
guneco <gu3fav@gmail.com>
2022-05-22 10:41:10 +0900

    improve title and subtitle

--- necoplot/slope_plot.py
+++ necoplot/slope_plot.py
@@ -22,8 +22,9 @@ class Slope(PlotBase):
         super().__init__(
             figsize=figsize, dpi=dpi, layout=layout, 
             show=show, **kwagrs)
-        self._xstart: float = 0.3
+        self._xstart: float = 0.2
         self._xend: float = 0.8
+        self._suffix: str = ''
         self._highlight: dict = {}
         
     def __enter__(self):
@@ -35,12 +36,13 @@ class Slope(PlotBase):
     def highlight(self, target_dict: dict):
         self._highlight.update(target_dict)
         
-    def config(self, xstart=0, xend=0):
+    def config(self, xstart=0, xend=0, suffix=''):
         self._xstart = xstart if xstart else self._xstart
         self._xend = xend if xend else self._xend
+        self._suffix = suffix if suffix else self._suffix
     
     def plot(self, time0, time1, names, xticks=(), title='',
-             xlabel='', ylabel=''):
+             subtitle=''):
         xticks = xticks if xticks else ('Before', 'After')
         
         xmin, xmax = 0, 4
@@ -48,25 +50,28 @@ class Slope(PlotBase):
         xend = xmax * self._xend
         ymax = max(*time0, *time1)
         ymin = min(*time0, *time1)
+        ytop = ymax * 1.2
         ybottom = ymin - (ymax * 0.2)
         yticks_position = ymin - (ymax * 0.1)
         text_args = {'verticalalignment':'center', 'fontdict':{'size':10}}
         
-        ax = self.fig.add_subplot(111, xlabel=xlabel, ylabel=ylabel)
+        ax = self.fig.add_subplot(111)
         
         for t0, t1, name in zip(time0, time1, names):
-            color = self._highlight.get(name, 'gray')
+            color = self._highlight.get(name, 'gray') if self._highlight else None
             plt.plot([xstart, xend], [t0, t1], lw=2, color=color, marker='o', markersize=5)
-            plt.text(xstart-0.1, t0, f'{name} {str(round(t0))}', horizontalalignment='right', **text_args)
-            plt.text(xend+0.1, t1, f'{str(round(t1))}', horizontalalignment='left', **text_args)
-            
-        ax.set_title(title, loc='left', fontsize=15)
+            plt.text(xstart-0.1, t0, f'{name} {str(round(t0))}{self._suffix}', horizontalalignment='right', **text_args)
+            plt.text(xend+0.1, t1, f'{str(round(t1))}{self._suffix}', horizontalalignment='left', **text_args)
         
         plt.xlim(xmin, xmax)
-        plt.ylim(ybottom, ymax*1.05)
+        plt.ylim(ybottom, ytop)
+    
+        plt.text(0, ytop, title, horizontalalignment='left', fontdict={'size':15})
+        plt.text(0, ytop*0.95, subtitle, horizontalalignment='left', fontdict={'size':10})
+        
         plt.text(xstart, yticks_position, xticks[0], horizontalalignment='center', **text_args)
         plt.text(xend, yticks_position, xticks[1], horizontalalignment='center', **text_args)
-        plt.subplots_adjust(top=0.2)
+        plt.text(xend, yticks_position, xticks[1], horizontalalignment='center', **text_args)
         plt.axis('off')
 
         
